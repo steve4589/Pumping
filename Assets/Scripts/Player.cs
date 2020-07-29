c25f5fb5991700a8140375160455ee291fe68aa4
Liberess <58577796+Liberess@users.noreply.github.com>
2020-07-29 15:31:57 +0900

    Add Sliding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,6 +33,8 @@ public class Player : MonoBehaviour
     //사망 효과음 On/Off
     private bool audioPlay = true;
 
+    private Animator animator;
+
     Animator anim;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
@@ -42,18 +44,16 @@ public class Player : MonoBehaviour
     //실시간 플레이어 위치
     Vector3 previousPosition = new Vector3();
 
-    public void Start()
-    {
-        gameManager.energyBar.value = 10f;
-    }
-
     private void Awake()
     {
+        gameManager.energyBar.value = 10f;
+
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -85,6 +85,8 @@ public class Player : MonoBehaviour
 
             Pumping();
 
+            Sliding();
+
             //Stop Speed
             if (Input.GetButtonUp("Horizontal"))
             {
@@ -97,7 +99,7 @@ public class Player : MonoBehaviour
             }
         }
 
-        //Animation
+        //Run Animation
         if (Mathf.Abs(rigid.velocity.x) < 0.3f)
         {
             anim.SetBool("isRun", false);
@@ -160,12 +162,25 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Sliding()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
+        {
+            animator.SetLayerWeight(1, 1);
+            anim.SetTrigger("doSliding");
+        }
+
+        Invoke("MoveSpeedReturn", 1.2f);
+    }
+
     private void Jump()
     {
         if (Input.GetButtonDown("Jump") && jumpCount < 2 && gameManager.energyBar.value >= 1f)
         {
             if (jumpCount < maxJump)
             {
+                animator.SetLayerWeight(1, 0);
+
                 rigid.velocity = new Vector2(rigid.velocity.x, jumpPower);
 
                 anim.SetBool("isJumpUp", true);
@@ -277,7 +292,7 @@ public class Player : MonoBehaviour
         else if(what == 5)
         {
             //Reaction Force
-            rigid.AddForce(new Vector2(dirc, 3f) * 5f, ForceMode2D.Impulse);
+            rigid.AddForce(new Vector2(dirc, 3f) * 4f, ForceMode2D.Impulse);
         }
 
         //Animation
